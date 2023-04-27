62c8d91bc4f3531fd61ad9f25ed3447ff2336d76
Hassan Ali <hassan.mern@gmail.com>
2023-04-27 08:22:24 +0500

    Sir Usman's Content added

---
+++ PHP/Examples/1.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+</head>
+<body>
+
+<h1> Introduction </h1>
+
+    <?php
+		echo("Server side Programming Language<br>");
+		echo "PHP: Personal Home Page <br>";
+		echo "Now, It stands for Hypertext Preprocessor <br>";
+		
+		echo "<input type='text' />";
+		
+		$arr =[1,2,3,'usman', true];
+		print_r($arr);
+		echo '<br>';
+		var_dump($arr);
+		echo '<br>';
+		
+		foreach ($arr as $a)
+		{
+			print "element is".$a;
+		}
+		//$b;
+		
+		echo '<br>';
+		echo '<br>';echo '<br>';
+		if(empty($b))
+		{
+			print "value set to variable";
+		}
+		
+					
+    ?>
+    
+
+</body>
+</html>
